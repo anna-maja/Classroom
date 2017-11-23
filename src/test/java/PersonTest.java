540a3e70c5a96f0693d8e4de2f7b349e524214b5
Anna-Maja Lithner <am_lithner@hotmail.com>
2017-11-23 10:06:47 +0100

    3rd commit. The tests for class Student021 moved to separate test class. Added the previously forgotten "@Test", which caused the earlier error messages.

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import otherclasses.Person021;
+import otherclasses.Student021;
 
 public class PersonTest {
 
@@ -11,5 +12,22 @@ public class PersonTest {
 		Person021 p = new Person021("Rafael", "Silva", 30,'M');
 		assertEquals(p.getFirstName().equals("Rafael"),true);
 	}
+	
+	@Test
+	public void testGetSetPerson021() {
+		
+		Student021 sBas1 = new Student021("Adam Bas", "Adamsson", 18, 'm');
+
+		sBas1.setFirstName("Adam Bas");
+		sBas1.setLastName("Adamsson");
+		sBas1.setAge(18);
+		sBas1.setGender('m');
+		assertEquals(sBas1.getFirstName().equals("Adam Bas"), true);
+		assertEquals(sBas1.getLastName().equals("Adamsson"), true);
+		assertEquals(sBas1.getAge(), 18);
+		assertEquals(sBas1.getGender(), 'm');
+	}
+
+
 
 }
--- src/test/java/Student021Test.java
+++ src/test/java/Student021Test.java
@@ -20,25 +20,11 @@ public class Student021Test {
 	Student021 sExt3 = new Student021("Clara Ext", "Carlsson", 40, 'f', 7, 7, 7);
 	Student021 sExt4 = new Student021("Delta Ext", "Dunker", 17, 'm', 0, 0, 0);
 
-	public void testGetSetPerson021() {
-		
-		Student021 sBas1 = new Student021("Adam Bas", "Adamsson", 18, 'm');
-
-		sBas1.setFirstName("Adam Bas");
-		sBas1.setLastName("Adamsson");
-		sBas1.setAge(18);
-		sBas1.setGender('m');
-		assertEquals(sBas1.getFirstName().equals("Adam Bas"), true);
-		assertEquals(sBas1.getLastName().equals("Adamsson"), true);
-		assertEquals(sBas1.getAge(), 18);
-		assertEquals(sBas1.getGender(), 'm');
-	}
-
-	
+	@Test
 	public void testGetSetStudent021() {
-		
+
 		Student021 sExt1 = new Student021("Albert Ext", "Andersson", 11, 'm', 6.6, 7.7, 9.9);
-		
+
 		sExt1.setFirstName("Albert Ext");
 		sExt1.setLastName("Andersson");
 		sExt1.setAge(11);
@@ -55,12 +41,7 @@ public class Student021Test {
 		assertEquals(sExt1.getSecondGrade(), 7.7, 0);
 		assertEquals(sExt1.getThirdGrade(), 9.9, 0);
 		assertEquals(sExt1.getAverageGrade(), 8.067, 0.5);
-		
-	}
 
-	
-	
-	
-	
+	}
 
 }
